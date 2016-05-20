ab685d03490f997114b50df595ca6ab5a17a4bb2
Nate Bosscher <natebosscher@Nates-MacBook-Pro.local>
2016-05-20 09:40:50 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # ReRunnable-SQL
+
+Takes SQL changes code and wraps each segment in IF EXISTS checks. See Notes for supported queries.
+
+run ```php sql-changes.php```
+
+Paste your sql code into the STDIN
+Something like
+```
+	ALTER TABLE [dbo].[MyTBL] ADD CONSTRAINT [FK_MyTBL_AnotherTBL] FOREIGN KEY (
+		[SomeID]
+	) REFERENCES [dbo].[AnotherTBL] (
+		[SomeID]
+	);
+	GO
+
+	--- next query...
+
+```
+
+
+Enter a new line with only a period on it
+
+A .sql file will open with the modified code.
+
+
+#### Note
+
+Curently setup for:
+
+1. FK and PK constraints
+2. DROP and CREATE Table
---
+++ sql-changes.php
@@ -0,0 +1,103 @@
+<?php
+
+function processLineSection($lines = array()){
+	$query = implode("\n\t", $lines);
+
+	if(preg_match("/^ALTER TABLE \[dbo\].\[([A-z0-9]+)\] (ADD|DROP) CONSTRAINT \[([A-z0-9\_]+)\]/", $lines[0], $matches)){
+
+		$tableName = $matches[1];
+		$type = $matches[2];
+		$constraint = $matches[3];
+		$constraintLookupLocation;
+
+		// type is foreign key
+		if(preg_match("/^FK_/", $constraint)){
+			$constraintLookupLocation = "REFERENTIAL_CONSTRAINTS";
+		}else if(preg_match("/^PK_/", $constraint)){
+			$constraintLookupLocation = "TABLE_CONSTRAINTS";
+		}else{
+			throw new Error("invalid type $constraint");
+		}
+
+		if($type == "ADD"){
+
+$out = "IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME='$constraint')
+BEGIN
+	$query;
+END";
+		}else if($type == "DROP"){
+
+	$out = "IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME='$constraint')
+BEGIN
+	$query;
+END";
+
+		}
+
+
+		return $out;
+	}
+
+	if(preg_match("/^CREATE TABLE \[dbo\].\[([A-z0-9]+)\]/", $lines[0], $matches)){
+
+		$tableName = $matches[1];
+
+		$cols = array();
+		foreach($lines as $k => $v){
+			if(preg_match("/\[([A-z0-9\_]+)\]\ \[[A-z0-9]+\].*?,$/", $v, $matches)){
+				array_push($cols, $matches[1]);
+			}
+		}
+		
+$out = "IF NOT EXISTS(SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE  TABLE_NAME = '$tableName')
+BEGIN
+		$query;
+END";
+		return $out;
+	}
+
+	if(preg_match("/DROP TABLE \[dbo\]\.\[([A-z0-9\_]+)\]$/", $lines[0], $matches)){
+
+		$tableName = $matches[1];
+		$out = "IF EXISTS(SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE  TABLE_NAME = '$tableName')
+BEGIN
+		$query;
+END";
+		return $out;
+	}
+
+	return $query;
+}
+
+$buff = array();
+$segments = array();
+
+do{
+	$line = trim(fgets(STDIN));
+
+	if($line == "GO" && count($buff) > 0){
+		array_push($segments, processLineSection($buff));
+		$buff = array();
+	}
+	
+	if($line != "" && $line != "GO") // skip blank lines
+		array_push($buff, $line);
+
+} while($line != ".");
+
+$fname = tempnam("/tmp", "sql-") . ".sql";
+$h = fopen($fname, "w");
+
+fwrite($h, "\n");
+fwrite($h, "--- GENERATED SQL\n\n");
+fwrite($h, implode("\nGO\n\n", $segments));
+fwrite($h, "\n\n--- END\n\n");
+
+fclose($h);
+
+exec("open $fname");
+
